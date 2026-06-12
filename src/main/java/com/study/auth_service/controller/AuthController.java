f6e791670929d54d00b7784a99bd445588ce9db6
mykyta <380630700519b@gmail.com>
2026-06-12 15:34:33 +0200

    Register

---
+++ src/main/java/com/study/auth_service/controller/AuthController.java
@@ -0,0 +1,16 @@
+package com.study.auth_service.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @PostMapping("/register")
+    public String registerUser(){
+        return "register";
+    }
+
+}
