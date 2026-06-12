a69fd58bc6b3074cc979a0cd054c3253af7fd824
mykyta <380630700519b@gmail.com>
2026-06-12 15:41:10 +0200

    LoginRequest

--- src/main/java/com/study/auth_service/controller/AuthController.java
+++ src/main/java/com/study/auth_service/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.study.auth_service.controller;
 
+import com.study.auth_service.DTO.request.LoginRequest;
 import com.study.auth_service.DTO.request.RegisterRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +17,7 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public String loginUser(){
+    public String loginUser(@RequestBody LoginRequest request){
         return "logged";
     }
 
