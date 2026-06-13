8dab549c93d566969d3125da235697f18ae6b26e
mykyta <380630700519b@gmail.com>
2026-06-13 11:38:23 +0200

    Add security config

---
+++ src/main/java/com/study/auth_service/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.study.auth_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filter(
+            org.springframework.security.config.annotation.web.builders.HttpSecurity http
+    ) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
